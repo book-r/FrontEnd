3f66b6066655e1f9af8a7e48a0344c62c2b84132
Arron M <marshall.arron@gmail.com>
2019-05-20 13:16:36 -0700

    Setup basic app wiring (Routing)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import AuthForm from './AuthForm';
-import FeaturedBook from './FeaturedBook';
 import Catalog from './Catalog';
+import FeaturedBook from './FeaturedBook';
+import BookDetail from './BookDetail';
 
 function App() {
   return (
     <div className="App">
       <AuthForm />
-      <FeaturedBook />
       <Catalog />
+
+      <Route
+        path='/book/:id'
+        component={BookDetail}
+      />
+      <Route
+        path='/featured/:id'
+        component={FeaturedBook}
+      />
     </div>
   );
 }

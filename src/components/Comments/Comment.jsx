b4bbdb5a9381c630772eb4207f0c7c1aad8710d8
Arron M <marshall.arron@gmail.com>
2019-05-22 14:22:09 -0700

    Fix a bug with user id check on Comment component

--- src/components/Comments/Comment.jsx
+++ src/components/Comments/Comment.jsx
@@ -5,7 +5,7 @@ import style from './Comments.module.scss';
 const Comment = (props) => {
   return (
     <div className={style.Comment}>
-      <span className={style.Comment__username}>{props.username} {props.userId === props.id && '(You)'}</span>
+      <span className={style.Comment__username}>{props.username} {props.userId === props.user_id && '(You)'}</span>
       <ReactStars
         className={style.Comment__rating}
         value={props.rating}

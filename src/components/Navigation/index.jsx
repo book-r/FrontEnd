83fa961f05bb1ad521f6ae05e602ef79c28998e7
Arron M <marshall.arron@gmail.com>
2019-05-22 19:37:52 -0700

    Add styles for navigation, set up color variables and font imports

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { checkAuth, logout } from '../../actions';
+import { logout } from '../../actions';
+import logo from '../../assets/bookr_logo_white.png';
 import style from './Navigation.module.scss';
 
 const Navigation = (props) => {
-  // props.checkAuth();
 
   const handleLogOut = () => {
-    // TODO: Refactor once auth is available on the backend
     props.logout();
     props.history.push('/');
   }
 
   return (
-    <nav>
-      <div>
-        Navigation Component
-      </div>
-      {
-        props.auth ? <span className={style.Logout} onClick={handleLogOut}>Log Out</span> : <Link to='/login'>Log In</Link>
-      }
-    </nav>
+    <div className={style.Navigation}>
+      <nav>
+        <div className={style.Navigation__logo}>
+          <img src={logo} alt='booker logo' />
+        </div>
+        {
+          props.auth && <span className={style.Logout} onClick={handleLogOut}>Log Out</span> // : <Link to='/login'>Log In</Link>
+        }
+      </nav>
+    </div>
   );
 }
 
@@ -30,4 +31,4 @@ const mapStateToProps = ({ auth }) => ({
   auth,
 });
  
-export default withRouter(connect(mapStateToProps, { checkAuth, logout })(Navigation));
+export default withRouter(connect(mapStateToProps, { logout })(Navigation));
